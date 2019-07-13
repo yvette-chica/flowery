84090353b68090726721214adf7f37f3a26e5a39
yvette <yvette@its6am.com>
2019-07-13 13:08:19 +0000

    Rm agenda component & fake data, refactor layouts)

---
+++ components/SiteLayout.js
@@ -0,0 +1,49 @@
+import { Component } from 'react'
+import { Layout, Menu, Icon } from 'antd'
+import Meta from './Meta'
+
+const { Header, Footer, Sider, Content } = Layout
+
+const SiteLayout = ({ children, collapsed, onCollapse, onMenuClick, headerContent }) => {
+    return (
+        <Layout>
+            <Meta />
+            <Header>
+                {headerContent}
+            </Header>
+            <Layout>
+                <Sider
+                    collapsed={collapsed}
+                    onCollapse={onCollapse}
+                    collapsible
+                    width={200}
+                    style={{ background: '#fff' }}
+                >
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        style={{ height: '100%', borderRight: 0 }}
+                        onClick={onMenuClick}
+                    >
+                        <Menu.Item key="1">
+                            <Icon type="profile"/>
+                            <span>Preview</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="edit"/>
+                            <span>Redux</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="usergroup-add"/>
+                            <span>Update Roles</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider> 
+                <Content>{children}</Content>
+            </Layout>
+            <Footer>Burning Toast © 2019</Footer>
+        </Layout>
+    )
+}
+
+export default SiteLayout 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
 import { Component } from 'react'
-import { Layout, Menu, Icon } from 'antd'
-import Agenda from '../components/agenda'
-import Meta from '../components/Meta'
-import agenda from '../fakeData/agenda'
 import Youtube from '../components/youtube'
-
+import Layout from '../components/SiteLayout'
 import { connect } from 'react-redux'
 import { startClock, serverRenderClock } from '../actions'
 import Examples from '../components/reduxExample'
 
-const { Header, Footer, Sider, Content } = Layout
 
 const views = {
     preview: '1',
@@ -20,7 +15,7 @@ const views = {
 class Index extends Component {
     state = {
         collapsed: false,
-        view: '3',
+        view: '1',
     }
 
     static getInitialProps ({ reduxStore, req }) {
@@ -58,9 +53,7 @@ class Index extends Component {
 
         if (this.state.view === views.preview) {
             agendaView = (
-                <Agenda
-                    agenda={agenda}
-                />
+                <div/>
             )
         } else if (this.state.view === views.edit) {
             agendaView = (
@@ -74,54 +67,12 @@ class Index extends Component {
         
         console.log('this.state', this.state)
         return (
-            <Layout>
-                <Meta />
-                <Header>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['1']}
-                        style={{ lineHeight: '64px' }}
-                    >
-                        <Menu.Item key="1">Agenda</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu>
-                </Header>
-                <Layout>
-                    <Sider
-                        collapsed={this.state.collapsed}
-                        onCollapse={this.onCollapse}
-                        collapsible
-                        width={200}
-                        style={{ background: '#fff' }}
-                    >
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['3']}
-                            defaultOpenKeys={['sub1']}
-                            style={{ height: '100%', borderRight: 0 }}
-                            onClick={this.onMenuClick}
-                        >
-                            <Menu.Item key="1">
-                                <Icon type="profile"/>
-                                <span>Preview</span>
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                                <Icon type="edit"/>
-                                <span>Redux</span>
-                            </Menu.Item>
-                            <Menu.Item key="3">
-                                <Icon type="usergroup-add"/>
-                                <span>Update Roles</span>
-                            </Menu.Item>
-                        </Menu>
-                    </Sider> 
-                    <Content>
-                        {agendaView}
-                    </Content>
-                </Layout>
-                <Footer>Burning Toast © 2019</Footer>
+            <Layout
+                collapsed={this.state.collapsed}
+                onCollapse={this.onCollapse}
+                onMenuClick={this.onMenuClick}
+            >
+                {agendaView}
             </Layout>
         )
     }
