b6c62f5c1c31b31a21139253be41e22fa3e516ce
yvette <yvette@its6am.com>
2019-07-15 00:01:19 +0000

    Rm counter components & reducers

--- rootReducer.js
+++ rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
-import counterReducer from './modules/counter'
 import videoDetail from './modules/videoDetail'
 import videoList from './modules/videoList'
 
 
 // REDUCERS
 export default combineReducers({
-    counterReducer,
     videoDetail,
     videoList,
 })
